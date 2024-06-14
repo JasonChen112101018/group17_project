8d25f0223f294b383d8971ff4a2bc0c4a2cf207d
JasonChen112101018 <jasonlittleqoo.md12@nycu.edu.tw>
2024-06-14 14:39:46 +0800

    modified zombie_painter.py

--- Pvz/zombie/zombie_painter.py
+++ Pvz/zombie/zombie_painter.py
@@ -1,10 +1,11 @@
 import pygame
-import ..painter
-import ..util.bus
-import .zombie
+import painter as painter
+from util.bus import Bus
+from zombie.head import Zombie_head
+from zombie.dead import Zombie_dead
 
 
-
+bus = Bus()
 # 繪製僵屍
 def paintZombies():
     for zombie in bus.zombies:
