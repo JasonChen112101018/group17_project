957f249d0ddb1d49b422eb04eb8488061208024f
JasonChen112101018 <jasonlittleqoo.md12@nycu.edu.tw>
2024-06-15 05:20:04 +0800

    modified zombie_actioner.py

--- Pvz/zombie_actioner.py
+++ Pvz/zombie_actioner.py
@@ -3,9 +3,18 @@ from zombie.normzombie import Zombie_normal
 from zombie.buckzombie import Zombie_bucket
 from zombie.head import Zombie_head
 from zombie.dead import Zombie_dead
-from plant.spikeweed import Spikeweed
-from util.bus import Bus
+import pygame, sys, random
+import painter as painter
+import actioner as actioner
+import mouseListener as mouseListener
 from settings import Setting
+from util.bus import Bus
+from sun import Sun
+import zombie 
+import zombie_actioner as z_actioner
+import zombie.zombie_painter as z_painter
+import plant.Plantpainter as plantpainter
+from plant.spikeweed import Spikeweed
 
 bus = Bus()
 sets = Setting()
@@ -24,13 +33,13 @@ def stepAction():
 def zombiesAction():
     bus.zombieIndex += 1
     if 7000 <= bus.globalTime <= 8000 or 14200 <= bus.globalTime <= 14400:
-        bus.zombieRate = 10
-    elif bus.globalTime <= 14200:
+        bus.zombieRate = 100
+    else:
         bus.zombieRate = 1000
 
     if bus.globalTime == 14300:
-        bus.zombies.append(Zombie_bucket(screen, sets.zombie_bucketImages))
         bus.endFlag = 1
+        
     if bus.globalTime < 14400:
         if bus.zombieIndex % bus.zombieRate == 0:
             type = random.randint(0, 20)
@@ -52,7 +61,7 @@ def hitAction():
             if zombie.headFlag is True:
                 zombie.images = sets.zombieLostHeadImages
                 zombie.headFlag = False
-        elif zombie.life <= 0:
+        elif zombie.life == 0:
             bus.zombies.remove(zombie)
             zombies_killed += 1  # Increment counter when zombie dies
 
@@ -66,11 +75,9 @@ def eat(zombie):
                     zombie.images = sets.zombieLostHeadAttackImages
                 elif zombie.life <= 5:
                     zombie.images = sets.normalAttackImages
-                elif zombie.life <= 8:
-                    zombie.images = sets.bucketAttackImages
                 else:
                     zombie.images = sets.bucketAttackImages
-                plant.life -= 0.5
+                plant.life -= 1
                 if plant.life <= 0:
                     bus.gridList[plant.gridX][plant.gridY] = -1
                     bus.paintPlants.remove(plant)
@@ -81,7 +88,7 @@ def eat(zombie):
                             zombie.images = sets.zombie_normalImages
                         else:
                             zombie.images = sets.zombie_bucketImages
-        elif isinstance(plant, Spikeweed):
+        if isinstance(plant, Spikeweed):
             if abs((plant.x + plant.width / 2) - (zombie.x + 20)) < 10 and zombie.y < plant.y - 40 and zombie.y + 140 > plant.y:
                 zombie.life -= 1
                 zombie.x += 0.05
@@ -92,7 +99,7 @@ def eat(zombie):
                     if zombie.headFlag is True:
                         zombie.images = sets.zombieLostHeadImages
                         bus.zombies.append(Zombie_head(screen, sets.zombieHeadImages, zombie.x, zombie.y))
-                elif zombie.life <= 0:
+                elif zombie.life == 0:
                     bus.zombies.append(Zombie_dead(screen, sets.zombieDieImages, zombie.x, zombie.y))
                     #zombies_killed += 1  # Increment counter when zombie dies
 
