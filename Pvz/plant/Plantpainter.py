324fcb3ddddd7fbb01ace920d7021e0d18724985
JasonChen112101018 <jasonlittleqoo.md12@nycu.edu.tw>
2024-06-14 13:34:41 +0800

    modified Plantpainter.py

--- Pvz/plant/Plantpainter.py
+++ Pvz/plant/Plantpainter.py
@@ -1,13 +1,18 @@
 import pygame
-from ..util import bus
+from util.bus import Bus
 
 
-    # 繪製植物
-    def paintPlants():
-        for plant in bus.paintPlants:
-            plant.blitme()
-    # 繪製子彈
-    def paintBullets():
-        for bullet in bus.bullets:
-            bullet.blitme()
+
+bus = Bus()
+
+# 繪製植物
+def paintPlants():
+    for plant in bus.paintPlants:
+        plant.blitme()
+
+# 繪製子彈
+def paintBullets():
+    for bullet in bus.bullets:
+        bullet.blitme()
+
 
