f6a2759fa681ca67aa93ad7a5f0cc6b1a484ae57
JasonChen112101018 <jasonlittleqoo.md12@nycu.edu.tw>
2024-06-15 02:16:50 +0800

    modified actioner.py

--- Pvz/actioner.py
+++ Pvz/actioner.py
@@ -17,7 +17,7 @@ def sunAction(bus, screen, sets):
     #陽光消失
     for i in range(len(bus.sunStay)):
         bus.sunStay[i].disappearTime += 1
-        if bus.sunStay[i].disappearTime == 300:
+        if bus.sunStay[i].disappearTime == 500:
             del bus.sunStay[i]
             break
 
