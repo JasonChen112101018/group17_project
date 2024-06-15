b953279a89e5da513e5a821f469cb633d83e8642
PulsarGlory <yuan112511272.ee12@nycu.edu.tw>
2024-06-15 23:02:31 +0800

    modify painter

--- Pvz/painter.py
+++ Pvz/painter.py
@@ -8,8 +8,10 @@ from  pygame.locals import *
 def initStartSurface(bus, screen, sets):
     window_size = screen.get_size()
     scaled_surface = pygame.transform.scale(sets.surface, window_size)
+    scaled_wood = pygame.transform.scale(sets.wood, (300, 200))
     scaled_beginBtn = pygame.transform.scale(sets.beginBtn, (300, 200))
     screen.blit(scaled_surface, (0,0))
+    screen.blit(scaled_wood, (200,100))
     screen.blit(scaled_beginBtn, (600,100))
 
 # 滑鼠拖曳植物
