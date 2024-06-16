772e77546ac002be941d7bda72efd5b31babb0bb
yummypenguin <timlin14012005.ee12@nycu.edu.tw>
2024-06-16 17:59:26 +0800

    modify mainGame.py

--- Pvz/mainGame.py
+++ Pvz/mainGame.py
@@ -183,8 +183,7 @@ def hitAction() :
              #   zombie_killed -= 64
               #  zombie_died += 1
             #elif zombie_killed % 334 == 0:
-             #   zombie_killed -= 334
-              #  zombie_died += 
+             
             bus.zombies.remove(zombie)
 
 # 殭屍吃植物
